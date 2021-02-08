7e3e7e2babdb1d5e7e3082df96cad058e3fd9528
RafikaAT <78097320+RafikaAT@users.noreply.github.com>
2021-02-08 15:53:23 +0000

    Exercise9_hw_pt1

---
+++ types.py
@@ -0,0 +1,8 @@
+first_name = "Rafika"
+last_name = "Turemis"
+print(first_name, last_name)
+my_full_name = [first_name, last_name]
+print(my_full_name)
+thesis_of_names = {'First Name':first_name, 'Last Name':last_name}
+print(thesis_of_names)
+print(thesis_of_names['First Name'])
